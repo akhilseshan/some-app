207161488e797c15130025f84731d2319b90e2b3
Akhil Seshan <akhilses@gmail.com>
2020-03-14 23:29:58 +0530

    Updates

---
+++ precache-manifest.6f11e5efcc9e708ab5a54235cd3f7848.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0c8b6b6903545a13ea8cb99d22454d",
+    "url": "/some-app/index.html"
+  },
+  {
+    "revision": "6901b33b840b75cf7e30",
+    "url": "/some-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "aa02ea101617fdc17309",
+    "url": "/some-app/static/js/2.48ed3ac9.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/some-app/static/js/2.48ed3ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6901b33b840b75cf7e30",
+    "url": "/some-app/static/js/main.e53a0c03.chunk.js"
+  },
+  {
+    "revision": "a823d2d4fc4976abd15a",
+    "url": "/some-app/static/js/runtime-main.765d94f7.js"
+  }
+]);
